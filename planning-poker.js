dc4d2b916d2707432e52187db73bcf78058eccef
Eric Zhu <erkangzhu@gmail.com>
2013-02-15 16:22:28 -0400

    improved ui

--- planning-poker.js
+++ planning-poker.js
@@ -3,6 +3,8 @@
 
 Players = new Meteor.Collection("players");
 Cards = new Meteor.Collection("cards");
+Messages = new Meteor.Collection("messages");
+CurrRoom = 1;
 
 CardNames = ['0', '1/2', '1', '2', '3', '5', '8', '13', '20', '40', '100', '?'];
 CardValues = [0, 1/2, 1, 2, 3, 5, 8, 13, 20, 40, 100, -1];
@@ -15,13 +17,13 @@ function getAllPlayers() {
 if (Meteor.isClient) {
   
   Session.set("current_player", null);
-  Session.set("current_card", null);
   
   Template.stage.players = getAllPlayers();
   
   Template.stage.events({
     'click input.clear-votes': function() {
         Players.update({}, {$set: {vote: NullCard, voted: false}}, {multi: true});
+        Messages.update({room: CurrRoom}, {$set: {content: Session.get("current_player") + " cleared all votes."}});
     }
   });
   
@@ -32,6 +34,10 @@ if (Meteor.isClient) {
     return true;
   };
   
+  Template.message.message = function() {
+    return Messages.findOne({room: CurrRoom});
+  };
+  
   Template.join_game.events({
     'click input.join': function(evt, tmpl) {
         var clientName = tmpl.find("#player-name-input").value;
@@ -39,11 +45,19 @@ if (Meteor.isClient) {
             alert("Player with the same name already exist!");
             return;
         }
+        if (!clientName) {
+          alert("Please enter your name!");
+          return;
+        }
         Players.insert({name: clientName, vote: NullCard, voted: false});
+        Messages.update({room: CurrRoom}, {$set: {content: clientName + " joined the game."}});
         Session.set("current_player", clientName);
-        Session.set("current_card", null);
     }
   });
+  
+  Template.join_game.curr_joined = function() {
+    return Session.get("current_player") != null ? false : true;
+  };
   
   Template.card_board.cards = function () {
     return Cards.find();
@@ -53,12 +67,15 @@ if (Meteor.isClient) {
     'click input.vote': function (evt, tmpl) {
       var currentPlayerName = Session.get("current_player");
       Players.update({name: currentPlayerName}, {$set: {vote: this, voted: true}});
-      Session.set("current_card", this.name);
     }
   });
   
   Template.card.selected = function() {
-    return Session.equals("current_card", this.name) ? "selected" : '';
+    var player = Players.findOne({name: Session.get("current_player")});
+    if (player) {
+      return player.vote.name == this.name ? 'selected' : '';
+    }
+    return '';
   };
   
   Template.current_players.players = getAllPlayers(); 
@@ -70,6 +87,10 @@ if (Meteor.isClient) {
   Template.player.events({
     'click input.remove-player': function () {
         Players.remove(this);
+        Messages.update({room: CurrRoom}, {$set: {content: this.name + " left the game."}});
+        if (Session.get("current_player") == this.name) {
+          Session.set("current_player", null);
+        }
     }
   });
 }
@@ -81,6 +102,9 @@ if (Meteor.isServer) {
         for (var i = 0; i < CardValues.length; i++) {
             Cards.insert({name: CardNames[i], value: CardValues[i]});
         }
+    }
+    if (Messages.find().count() === 0) {
+      Messages.insert({content: "Welcome to Planning Poker!", room: CurrRoom});
     }
   });
 }
